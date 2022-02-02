907fdbdbf4af6f3da11be1520ac2326c7dc4a165
Marlow Collins <marlowcollins95@gmail.com>
2022-02-01 22:43:41 -0600

    CRUD Routes Finished

--- CatalogApi/Controllers/ItemsController.cs
+++ CatalogApi/Controllers/ItemsController.cs
@@ -40,5 +40,61 @@ namespace Catalog.Controllers
             }
             return item.AsDto();
         }
+
+    //Post /items
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new(){
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto()); 
+        }
+
+        //PUT /items
+        [HttpPut("{id}")]
+        
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItems(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            //record types support with expressions
+            Item updatedItem = existingItem with {
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+            };
+
+            repository.UpdateItem(updatedItem);
+
+            return NoContent();
+        }
+
+        //DELETE/items/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItems(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+
+            repository.DeleteItem(id);
+
+            return NoContent();
+        }
     }
 }
