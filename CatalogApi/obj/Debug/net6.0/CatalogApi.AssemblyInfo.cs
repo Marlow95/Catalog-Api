18fca6146c7e5b3174051a0f507c21d0d59fb549
Marlow Collins <marlowcollins95@gmail.com>
2022-02-04 01:47:13 -0600

    Secrets and Health Checks Added

--- CatalogApi/obj/Debug/net6.0/CatalogApi.AssemblyInfo.cs
+++ CatalogApi/obj/Debug/net6.0/CatalogApi.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("74ac69c3-f61b-42dc-99d3-40705d3711e2")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
